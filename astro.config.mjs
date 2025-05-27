1189a61ba83fa5142a9835415a1276ae151a66c6
kukky44 <yusuke122644@gmail.com>
2025-05-28 03:01:17 +1200

    fix issue on action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kukky44.github.io/binary-search-tree',
+  site: 'https://kukky44.github.io',
+  base: '/binary-search-tree',
 });
